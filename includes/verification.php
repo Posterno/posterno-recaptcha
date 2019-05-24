e15e149e5978a39869623dd107f174719f29b29a
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-05-24 12:22:41 +0100

    validate recaptcha

--- includes/verification.php
+++ includes/verification.php
@@ -13,9 +13,34 @@ use PNO\Exception;
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Validate recaptcha response.
+ *
+ * @throws Exception When validation fails.
+ * @return void
+ */
 function pno_recaptcha_verify_submission() {
 
-	throw new Exception( 'hehehe', 'recaptcha-validation-error' );
+	$secret_key = pno_get_option( 'recaptcha_secret_key', false );
+
+	if ( $secret_key && isset( $_POST['g-recaptcha-response'] ) ) {
+		$recaptcha              = new \ReCaptcha\ReCaptcha( $secret_key );
+		$recaptcha_response_key = esc_html( $_POST['g-recaptcha-response'] );
+		$resp                   = $recaptcha->verify( $recaptcha_response_key, $_SERVER['REMOTE_ADDR'] );
+		if ( ! $resp->isSuccess() ) {
+			throw new Exception( esc_html__( 'Recaptcha validation failed.', 'posterno-recaptcha' ), 'recaptcha-validation-error' );
+		}
+	}
 
 }
-add_action( 'pno_before_user_login', 'pno_recaptcha_verify_submission' );
+
+// Hook into the allowed forms.
+$locations = pno_get_option( 'recaptcha_location', [] );
+
+if ( is_array( $locations ) && ! empty( $locations ) ) {
+	if ( in_array( 'login', $locations, true ) ) {
+		add_action( 'pno_before_user_login', 'pno_recaptcha_verify_submission' );
+	}
+}
+
+
